e74a880282ff17915065ee725b9a8891b7cba746
Gaëtan Blaise-Cazalet <gaetan.blaisecazalet@gmail.com>
2023-09-15 02:49:37 +0200

    Integrating ECS (not finished)

--- 06_dungeon_crawl/src/main.rs
+++ 06_dungeon_crawl/src/main.rs
@@ -1,28 +1,34 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 mod map;
-mod player;
 mod map_builder;
 mod camera;
+mod components;
+mod spawner;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use legion::*;
+    pub use legion::world::SubWorld;
+    pub use legion::systems::CommandBuffer;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+
     pub use crate::map::*;
-    pub use crate::player::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::spawner::*;
 }
 
 use prelude::*;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs : World,
+    resources: Resources,
+    systems: Schedule,
 }
 
 impl GameState for State {
@@ -31,9 +37,8 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+
+
     }
 }
 
@@ -41,10 +46,15 @@ impl State {
     fn new() -> Self {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        spawn_player(&mut ecs, map_builder.player_start);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start),
+            ecs,
+            resources,
+            systems: build_scheduler(),
         }
     }
 }
